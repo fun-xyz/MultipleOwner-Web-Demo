976ffc8ccb02e40c1565778c5f01f28351f3d297
aaronchewbani <aaronchewbani@gmail.com>
2023-08-30 16:20:30 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './MultiOwnerExample';
+import App from './app';
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
